989e93d236941807d0cfc5d399da83ad5b919b8a
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2025-01-05 17:05:07 +0700

    Update middleware.ts

--- src/utils/auth/middleware.ts
+++ src/utils/auth/middleware.ts
@@ -1,17 +1,21 @@
 import { NextMiddleware, NextResponse } from 'next/server';
-import { middleware_hasServerSession } from './server';
+import { middleware_hasServerSession } from './server'; // Impor sesi dari server
+import { middleware_hasGoogleServerSession } from './googleServer'; // Impor sesi dari Google
 
 export const withAuth = (authUrl: string, middleware: NextMiddleware): NextMiddleware => {
   return (req, evt) => {
-    const loggedin = middleware_hasServerSession(req);
+    // Cek sesi dari server dan Google
+    const loggedIn = middleware_hasServerSession(req) || middleware_hasGoogleServerSession(req);
 
-    if (!loggedin) {
+    // Jika tidak ada sesi yang valid, redirect ke halaman autentikasi
+    if (!loggedIn) {
       const url = req.nextUrl.clone();
       url.pathname = authUrl;
 
       return NextResponse.redirect(url);
     }
 
+    // Jika ada sesi yang valid, lanjutkan dengan middleware berikutnya
     return middleware(req, evt);
   };
 };
