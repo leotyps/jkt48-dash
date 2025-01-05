09b6570ee032a0294a310976f2f666ec149d67bc
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2025-01-05 15:00:39 +0700

    Create setSession.ts

---
+++ src/pages/api/auth/setSession.ts
@@ -0,0 +1,16 @@
+// pages/api/auth/setSession.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+import { setServerSession } from '@/utils/auth/server'; // Import setServerSession
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const userSession = req.body;
+    
+    // Simpan sesi menggunakan setServerSession
+    setServerSession(userSession, req, res);
+    
+    return res.status(200).json({ success: true });
+  } else {
+    res.status(405).json({ success: false, message: 'Method Not Allowed' });
+  }
+}
