e25e870e296abc9aff5318d76ae0dd6bd0850ec4
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2025-01-12 16:46:15 +0700

    Update StyledChart.tsx

--- src/components/chart/StyledChart.tsx
+++ src/components/chart/StyledChart.tsx
@@ -39,6 +39,7 @@ export function StyledChart(props: ChartProps) {
       strokeDashArray: 0,
       fillOpacity: 1,
       discrete: [],
+      radius: 2,
       shape: 'circle',
       offsetX: 0,
       offsetY: 0,
