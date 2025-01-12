146cf4172324fdf05617d02d64c3ecef1f716f3a
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2025-01-12 13:39:08 +0700

    Update send-email.ts

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { nodemailer } from "nodemailer";
+import nodemailer from "nodemailer";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
