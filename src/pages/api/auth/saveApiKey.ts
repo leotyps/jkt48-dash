c40bd2394b46c08eb006564fd0142beabfea1f47
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2025-01-15 22:41:50 +0700

    Update saveApiKey.ts

--- src/pages/api/auth/saveApiKey.ts
+++ src/pages/api/auth/saveApiKey.ts
@@ -1,8 +1,8 @@
 // pages/api/saveApiKey.ts
+
 import { NextApiRequest, NextApiResponse } from "next";
 import { Pool } from "pg";
 
-// Initialize PostgreSQL client for CockroachDB
 const pool = new Pool({
   connectionString:
     "postgresql://jkt48connect_apikey:vAgy5JNXz4woO46g8fho4g@jkt48connect-7018.j77.aws-ap-southeast-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
@@ -19,11 +19,9 @@ const saveApiKey = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
       const client = await pool.connect();
 
-      // Log SQL query and parameters for debugging
       console.log("Inserting API key data into the database");
       console.log(`apiKey: ${apiKey}, expiryDate: ${expiryDate}, limit: ${limit}`);
 
-      // Insert API key data into the database
       const result = await client.query(
         `INSERT INTO api_keys (api_key, expiry_date, remaining_requests, max_requests, last_access_date, seller)
          VALUES ($1, $2, $3, $4, NOW(), $5) RETURNING api_key`,
@@ -38,7 +36,6 @@ const saveApiKey = async (req: NextApiRequest, res: NextApiResponse) => {
 
       client.release();
 
-      // Check if the insertion was successful
       if (result.rows.length > 0) {
         return res.status(200).json({
           message: "API Key successfully created",
@@ -48,9 +45,14 @@ const saveApiKey = async (req: NextApiRequest, res: NextApiResponse) => {
       } else {
         return res.status(500).json({ message: "Failed to save API key." });
       }
-    } catch (error) {
-      console.error("Error saving API Key:", error); // Log error details
-      return res.status(500).json({ message: `Internal server error: ${error.message}` });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error("Error saving API Key:", error); // Log error details
+        return res.status(500).json({ message: `Internal server error: ${error.message}` });
+      } else {
+        console.error("Unknown error:", error);
+        return res.status(500).json({ message: "Unknown error occurred." });
+      }
     }
   } else {
     return res.status(405).json({ message: "Method Not Allowed" });
