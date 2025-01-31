57a09e3d7c5e72cba42ecc9cc6540975bc1b108c
Valzyy <jkt48connect@gmail.com>
2025-01-31 16:01:52 +0000

    Update PaymentView.tsx

--- src/config/example/PaymentView.tsx
+++ src/config/example/PaymentView.tsx
@@ -19,40 +19,44 @@ export default function PaymentList() {
       {/* Card QRIS */}
       <Box
         bg="blue.400"
-        p={6}
+        p={8}
         borderRadius="lg"
         display="flex"
         justifyContent="space-between"
         alignItems="center"
         cursor="pointer"
-        onClick={() => handlePaymentClick("qris")}
+        w="100%"
+        h="100px"
         _hover={{ bg: "blue.500" }}
+        onClick={() => handlePaymentClick("qris")}
       >
-        <Text fontSize="xl" fontWeight="bold" color="white">
+        <Text fontSize="2xl" fontWeight="bold" color="white">
           QRIS
         </Text>
-        <Image src="https://8030.us.kg/file/MkZ4yUJAu6Zd.png" alt="QRIS Logo" boxSize="60px" />
+        <Image src="https://8030.us.kg/file/MkZ4yUJAu6Zd.png" alt="QRIS Logo" boxSize="80px" />
       </Box>
 
       {/* Card PayPal */}
       <Box
         bg="gray.400"
-        p={6}
+        p={8}
         borderRadius="lg"
         display="flex"
         justifyContent="space-between"
         alignItems="center"
+        w="100%"
+        h="100px"
         opacity={0.7} // PayPal masih Coming Soon
       >
         <Flex align="center">
-          <Text fontSize="xl" fontWeight="bold" mr={3}>
+          <Text fontSize="2xl" fontWeight="bold" mr={3}>
             PayPal
           </Text>
-          <Tag colorScheme="red" size="md">
+          <Tag colorScheme="red" size="lg">
             Coming Soon
           </Tag>
         </Flex>
-        <Image src="https://8030.us.kg/file/IIqCNN4mm3br.png" alt="PayPal Logo" boxSize="60px" />
+        <Image src="https://8030.us.kg/file/IIqCNN4mm3br.png" alt="PayPal Logo" boxSize="80px" />
       </Box>
     </VStack>
   );
