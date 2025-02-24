6f5108bacbbccf69f567307c3811e16e87573c33
Valzyy <jkt48connect@gmail.com>
2025-02-24 19:18:56 +0700

    Update db2.ts

--- src/utils/db2.ts
+++ src/utils/db2.ts
@@ -1,22 +1,26 @@
-// utils/db.ts
-
 import { MongoClient } from "mongodb";
 
-const MONGO_URL = process.env.MONGO_URL || "mongodb+srv://jkt48connect:i6SDcf2nFcqewY2i@jkt48connect.i1ksz.mongodb.net/?retryWrites=true&w=majority&appName=jkt48connect";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://jkt48connect:<db_password>@jkt48connect.i1ksz.mongodb.net/?retryWrites=true&w=majority&appName=jkt48connect";
+const MONGODB_DB = "jkt48connect"; // Nama database yang digunakan
 
-if (!MONGO_URL) {
-  throw new Error("❌ Database URL (MONGO_URL) tidak ditemukan di environment variables!");
+if (!MONGODB_URI) {
+  throw new Error("❌ MongoDB URI tidak ditemukan di environment variables.");
 }
 
-let client: MongoClient;
-let db: any;
+let cachedClient: MongoClient | null = null;
 
+/**
+ * Fungsi untuk menghubungkan ke MongoDB
+ */
 export const connectToDatabase = async () => {
-  if (!client) {
-    client = new MongoClient(MONGO_URL);
-    await client.connect();
-    console.log("✅ Terhubung ke MongoDB");
-    db = client.db(); // Gunakan default database dari URL
+  if (cachedClient) {
+    return cachedClient.db(MONGODB_DB);
   }
-  return db;
+
+  const client = new MongoClient(MONGODB_URI);
+  await client.connect();
+  console.log("✅ Terhubung ke MongoDB");
+  
+  cachedClient = client;
+  return client.db(MONGODB_DB);
 };
