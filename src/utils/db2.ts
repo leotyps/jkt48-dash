3602ddebd031aac00cfcb10f5234aac037fb8389
Valzyy <jkt48connect@gmail.com>
2025-02-25 14:21:31 +0700

    Update db2.ts

--- src/utils/db2.ts
+++ src/utils/db2.ts
@@ -1,26 +1,15 @@
-import { MongoClient } from "mongodb";
+import { Pool } from "pg";
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://jkt48connect:<db_password>@jkt48connect.i1ksz.mongodb.net/?retryWrites=true&w=majority&appName=jkt48connect";
-const MONGODB_DB = "jkt48connect"; // Nama database yang digunakan
-
-if (!MONGODB_URI) {
-  throw new Error("❌ MongoDB URI tidak ditemukan di environment variables.");
-}
-
-let cachedClient: MongoClient | null = null;
+const pool = new Pool({
+  connectionString: process.env.POSTGRESQL_URL, // Pastikan ada di .env
+  ssl: {
+    rejectUnauthorized: false, // Gunakan jika PostgreSQL membutuhkan SSL
+  },
+});
 
 /**
- * Fungsi untuk menghubungkan ke MongoDB
+ * Fungsi untuk menghubungkan ke database PostgreSQL
  */
 export const connectToDatabase = async () => {
-  if (cachedClient) {
-    return cachedClient.db(MONGODB_DB);
-  }
-
-  const client = new MongoClient(MONGODB_URI);
-  await client.connect();
-  console.log("✅ Terhubung ke MongoDB");
-  
-  cachedClient = client;
-  return client.db(MONGODB_DB);
+  return pool;
 };
