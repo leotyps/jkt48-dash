30124206476e98a25c6298d54e334693e6669bf3
Valzyy <jkt48connect@gmail.com>
2025-04-19 19:45:01 +0700

    Update save-data.ts

--- src/pages/api/auth/save-data.ts
+++ src/pages/api/auth/save-data.ts
@@ -5,7 +5,7 @@ import { connectToDatabase } from '@/utils/db'; // Koneksi ke database
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const { team_id, saldo = 0, apikey = null } = req.query;
+    const { team_id, saldo = 0, apikey = null, phone_number = null } = req.query;
 
     if (!team_id) {
       return res.status(400).json({ error: 'team_id is required' });
@@ -13,11 +13,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const db = await connectToDatabase();
     const query = `
-      INSERT INTO zenova (team_id, saldo, apikey)
-      VALUES ($1, $2, $3)
+      INSERT INTO zenova (team_id, saldo, apikey, phone_number)
+      VALUES ($1, $2, $3, $4)
       ON CONFLICT (team_id) DO NOTHING;
     `;
-    const values = [team_id, saldo, apikey];
+    const values = [team_id, saldo, apikey, phone_number];
     await db.query(query, values);
 
     res.status(200).json({ message: 'User data saved successfully' });
